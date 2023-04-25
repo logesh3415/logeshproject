85b6a9280b5c6e9b3c699d3ce3d97c9e7fddd753
logesh3415 <115616014+logesh3415@users.noreply.github.com>
2023-04-25 10:28:00 +0530

    Add files via upload

---
+++ restapi1/src/main/java/com/example/demo/Repository/CumRepo.java
@@ -0,0 +1,13 @@
+package com.example.demo.Repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.example.demo.Model.CustomerDetails;
+
+
+@Repository
+public interface CumRepo extends JpaRepository<CustomerDetails, Integer>
+{
+	
+}
